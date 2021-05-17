531d675aecdec2627bd7a4766849c07c24a9894f
imjoseangel <josea.munoz@gmail.com>
2021-05-17 11:25:11 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,97 @@
+#-------------------------------
+# Local Declarations
+#-------------------------------
+locals {
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+}
+
+data "azurerm_client_config" "current" {}
+
+#---------------------------------------------------------
+# Resource Group Creation or selection - Default is "false"
+#----------------------------------------------------------
+data "azurerm_resource_group" "rgrp" {
+  count = var.create_resource_group == false ? 1 : 0
+  name  = var.resource_group_name
+}
+
+resource "azurerm_resource_group" "rg" {
+  count    = var.create_resource_group ? 1 : 0
+  name     = lower(var.resource_group_name)
+  location = var.location
+  tags     = merge({ "ResourceName" = format("%s", var.resource_group_name) }, var.tags, )
+}
+
+
+#---------------------------------------------------------
+# Kubernetes Creation or selection
+#---------------------------------------------------------
+resource "azurerm_kubernetes_cluster" "main" {
+  name                            = format("%s-%s", var.prefix, lower(replace(var.name, "/[[:^alnum:]]/", "")))
+  location                        = var.location
+  resource_group_name             = local.resource_group_name
+  node_resource_group             = var.node_resource_group
+  dns_prefix                      = var.prefix
+  api_server_authorized_ip_ranges = var.authorized_ips
+  sku_tier                        = var.sku_tier
+  private_cluster_enabled         = var.private_cluster_enabled
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 3
+    vm_size             = var.default_vm_size
+    enable_auto_scaling = true
+    vnet_subnet_id      = var.vnet_subnet_id
+    type                = "VirtualMachineScaleSets"
+    availability_zones  = var.availability_zones
+    max_count           = var.max_default_node_count
+    min_count           = var.min_default_node_count
+    os_disk_size_gb     = 128
+  }
+
+  linux_profile {
+    admin_username = "k8s"
+
+    ssh_key {
+      key_data = file(var.public_ssh_key)
+    }
+  }
+  addon_profile {
+    oms_agent {
+      enabled = false
+    }
+
+    http_application_routing {
+      enabled = false
+    }
+
+    ingress_application_gateway {
+      enabled    = var.create_ingress
+      gateway_id = var.gateway_id
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin    = var.network_plugin
+    network_policy    = var.network_policy
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = merge({ "ResourceName" = format("%s-%s", var.prefix, lower(replace(var.name, "/[[:^alnum:]]/", ""))) }, var.tags, )
+
+  lifecycle {
+    ignore_changes = [
+      default_node_pool[0].node_count
+    ]
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,114 @@
+variable "name" {
+  description = "Name of Azure Kubernetes service."
+}
+
+variable "create_resource_group" {
+  description = "Whether to create resource group and use it for all networking resources"
+  default     = false
+}
+
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+  default     = "rg-demo-westeurope-01"
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default     = "westeurope"
+}
+
+variable "prefix" {
+  description = "The prefix for the resources created in the specified Azure Resource Group"
+  type        = string
+  default     = "default"
+}
+
+variable "default_vm_size" {
+  description = "The size of the Virtual Machine, such as Standard_D2_v4"
+  type        = string
+  default     = "Standard_B2ms"
+}
+
+variable "availability_zones" {
+  description = "A list of Availability Zones across which the Node Pool should be spread. Changing this forces a new resource to be created."
+  type        = list(string)
+  default     = ["1", "2", "3"]
+}
+
+variable "sku_tier" {
+  description = "The SKU Tier that should be used for this Kubernetes Cluster. Possible values are Free and Paid"
+  type        = string
+  default     = "Free"
+}
+
+variable "private_cluster_enabled" {
+  description = "If true cluster API server will be exposed only on internal IP address and available only in cluster vnet."
+  type        = bool
+  default     = false
+}
+
+variable "authorized_ips" {
+  description = "A list of IP addresses to allow to connect to the cluster."
+  type        = list(string)
+  default     = null
+}
+
+variable "max_default_node_count" {
+  description = "(Required) The maximum number of nodes which should exist in this Node Pool. If specified this must be between 1 and 1000."
+  type        = number
+  default     = 15
+}
+
+variable "min_default_node_count" {
+  description = "(Required) The minimum number of nodes which should exist in this Node Pool. If specified this must be between 1 and 1000."
+  type        = number
+  default     = 3
+}
+
+variable "node_resource_group" {
+  description = "(Optional) The name of the Resource Group where the Kubernetes Nodes should exist. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
+
+variable "network_plugin" {
+  description = "Network plugin to use for networking."
+  type        = string
+  default     = "kubenet"
+}
+
+variable "network_policy" {
+  description = "Network policy to use for networking."
+  type        = string
+  default     = null
+}
+
+variable "public_ssh_key" {
+  description = "A custom ssh key to control access to the AKS cluster"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "create_ingress" {
+  description = "Deploy the Application Gateway ingress controller to this Kubernetes Cluster"
+  type        = bool
+  default     = false
+}
+
+variable "gateway_id" {
+  description = "(Optional) The ID of the Application Gateway to integrate with the ingress controller of this Kubernetes Cluster"
+  type        = string
+  default     = null
+}
+
+variable "vnet_subnet_id" {
+  description = "(Optional) The ID of a Subnet where the Kubernetes Node Pool should exist. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default     = {}
+}
