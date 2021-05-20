a762c376ec2bc0f88cf01cbdf7def1e06f0f21c3
imjoseangel <josea.munoz@gmail.com>
2021-05-20 09:30:03 +0200

    remove module

--- main.tf
+++ main.tf
@@ -27,9 +27,15 @@ resource "azurerm_resource_group" "rg" {
 # SSH Key Creation or selection
 #---------------------------------------------------------
 
-module "ssh-key" {
-  source         = "./modules/ssh-key"
-  public_ssh_key = var.public_ssh_key == "" ? "" : var.public_ssh_key
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "private_key" {
+  count    = var.public_ssh_key == "" ? 1 : 0
+  content  = tls_private_key.ssh.private_key_pem
+  filename = "private_ssh_key"
 }
 
 #---------------------------------------------------------
