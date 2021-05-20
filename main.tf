41de6b86006bc51988cf688d428c38123e89ee7b
imjoseangel <josea.munoz@gmail.com>
2021-05-20 10:13:39 +0200

    keep back private key

--- main.tf
+++ main.tf
@@ -32,11 +32,11 @@ resource "tls_private_key" "ssh" {
   rsa_bits  = 2048
 }
 
-resource "local_file" "private_key" {
-  count    = var.public_ssh_key == "" ? 1 : 0
-  content  = tls_private_key.ssh.private_key_pem
-  filename = "private_ssh_key"
-}
+# resource "local_file" "private_key" {
+#   count    = var.public_ssh_key == "" ? 1 : 0
+#   content  = tls_private_key.ssh.private_key_pem
+#   filename = "private_ssh_key"
+# }
 
 #---------------------------------------------------------
 # Kubernetes Creation or selection
--- modules/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "public_ssh_key" {
-  description = "An ssh key set in the main variables of the terraform-azurerm-aks module"
-  default     = ""
-}
