471d23fe796e280a837f7c9ae6d80ac8cada7472
imjoseangel <josea.munoz@gmail.com>
2021-08-09 18:28:55 +0200

    add resource

--- main.tf
+++ main.tf
@@ -161,8 +161,7 @@ resource "azurerm_log_analytics_workspace" "main" {
   resource_group_name = local.resource_group_name
   sku                 = var.log_analytics_workspace_sku
   retention_in_days   = var.log_retention_in_days
-
-  tags = var.tags
+  tags                = var.tags
 }
 
 resource "azurerm_log_analytics_solution" "main" {
