203889cb4cbe7c5ba14927a3d0780d50bf25e3af
imjoseangel <josea.munoz@gmail.com>
2022-02-14 23:04:16 +0100

    enable default pc

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "sku_tier" {
 variable "private_cluster_enabled" {
   description = "If true cluster API server will be exposed only on internal IP address and available only in cluster vnet."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "private_cluster_public_fqdn_enabled" {
