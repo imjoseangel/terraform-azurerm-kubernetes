d4d31d44a66915f37a082dda57ea6573f3e5531b
imjoseangel <josea.munoz@gmail.com>
2022-10-08 16:44:53 +0200

    add type to vars

--- variables.tf
+++ variables.tf
@@ -1,20 +1,24 @@
 variable "name" {
   description = "Name of Azure Kubernetes service."
+  type        = string
 }
 
 variable "create_resource_group" {
   description = "Whether to create resource group and use it for all networking resources"
   default     = true
+  type        = bool
 }
 
 variable "resource_group_name" {
   description = "A container that holds related resources for an Azure solution"
   default     = "rg-demo-westeurope-01"
+  type        = string
 }
 
 variable "location" {
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
   default     = "westeurope"
+  type        = string
 }
 
 variable "prefix" {
